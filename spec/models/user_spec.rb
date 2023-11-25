554307cdd23df4cfbe80dd58f98a8144227789fa
ko-shima <xxghapxx.poomomo@gmail.com>
2023-11-25 17:08:41 +0900

    コードレビュー修正②

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -49,17 +49,17 @@ RSpec.describe User, type: :model do
       it '英字のみのパスワードでは登録できない' do
         @user.password = 'aaaaaa'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password", "Password には英字と数字の両方を含めて設定してください")
       end
       it '数字のみのパスワードでは登録できない' do
         @user.password = '111111'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password", "Password には英字と数字の両方を含めて設定してください")
       end
       it '全角文字を含むパスワードでは登録できない' do
         @user.password = '1aaaaaa１'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password", "Password には英字と数字の両方を含めて設定してください")
       end
       it 'passwordとpassword_confirmationが不一致では登録できない' do
         @user.password_confirmation = 'asdfgh1'
