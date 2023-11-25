58288d0e03c4557a80bdeb60b75bcc507a45a812
ko-shima <xxghapxx.poomomo@gmail.com>
2023-11-25 15:06:58 +0900

    factorybot, faker導入

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials}
+    email                 {Faker::Internet.email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_nam              {'田中'}
+    first_name            {'太郎'}
+    last_name_kana        {'タナカ'}
+    first_name_kana       {'タロウ'}
+    birthday              {Faker::Date.birthday}
+  end
+end
+
