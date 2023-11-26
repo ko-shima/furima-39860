036e401266c074ea8773d166a1c78bd639a645ae
ko-shima <xxghapxx.poomomo@gmail.com>
2023-11-26 14:49:34 +0900

    ActiveHash導入・アソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,13 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :region
+  belongs_to :shipping_data
   belongs_to :user
   has_one_attached :image
 
+  validates :item_name, :description, :price, presence: true
+  validates :category_id, :condition_id, :postage_id, :region_id, :shipping_data, numericality: { other_than: 1 } 
 end
