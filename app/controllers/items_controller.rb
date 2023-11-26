57363802306b3615975841ffdfd12ad80b551cc7
ko-shima <xxghapxx.poomomo@gmail.com>
2023-11-26 10:53:30 +0900

    Active Storage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,9 @@ class ItemsController < ApplicationController
 
   def index
   end
+
+  def item_params
+    params.require(:item).permit(:item_name, :description, :price, :category_id, :condition_id, :postage_id, :region_id, :shipping_data_id, :image).merge(:user_id: current_user.id)
+  end
+
 end
