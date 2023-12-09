4d5a62f6b110c781b8b6558af22f0f9a92252921
ko-shima <xxghapxx.poomomo@gmail.com>
2023-12-09 15:46:08 +0900

    アップデートアクションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :move_to_index, only: :edit
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -26,10 +27,28 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:item_name, :description, :price, :category_id, :condition_id, :postage_id, :region_id, :shipping_data_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    item = Item.find(params[:id])
+    user_id = item.user_id
+
+    unless user_signed_in? && current_user.id == user_id
+      redirect_to action: :index
+    end
   end
 
 end
