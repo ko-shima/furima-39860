2875a69fe5b93d5fc26cf94c1817006159c61b86
ko-shima <xxghapxx.poomomo@gmail.com>
2023-12-17 18:43:53 +0900

    Formオブジェクトの作成

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -1,2 +1,17 @@
 class OrderShipping
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :region_id, :city, :block, :building, :phone_number, :order
+
+  validates :user_id, presence: true
+  validates :item_id, presence: true
+  validates :postal_code, presence: true
+  validates :region_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :city, presence: true
+  validates :block, presence: true
+  validates :phone_number, presence: true
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    shipping.create(postal_code: postal_code, region_id: region_id, city: city, block: block, building: building, phone_number: phone_number)
+  end
 end
--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -3,11 +3,11 @@ class Shipping < ApplicationRecord
   belongs_to :order
   belongs_to :region
 
-  validates :postal_code, presence: true
-  validates :region_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :city, presence: true
-  validates :block, presence: true
-  validates :phone_number, presence: true
+  #validates :postal_code, presence: true
+  #validates :region_id, numericality: { other_than: 1, message: "can't be blank" }
+  #validates :city, presence: true
+  #validates :block, presence: true
+  #validates :phone_number, presence: true
 
   
 end
