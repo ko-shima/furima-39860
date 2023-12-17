8ef1e6be0014c40cce9ca5cc2dd85de5bea25cec
ko-shima <xxghapxx.poomomo@gmail.com>
2023-12-17 17:50:03 +0900

    orderモデル・shippingモデル生成

--- db/migrate/20231217084203_create_orders.rb
+++ db/migrate/20231217084203_create_orders.rb
@@ -1,9 +1,8 @@
-class CreateOders < ActiveRecord::Migration[7.0]
+class CreateOrders < ActiveRecord::Migration[7.0]
   def change
-    create_table :oders do |t|
+    create_table :orders do |t|
       t.references :user, null: false, foreign_key: true
       t.references :item, null: false, foreign_key: true
-
       t.timestamps
     end
   end
