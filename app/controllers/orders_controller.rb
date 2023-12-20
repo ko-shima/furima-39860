1b6d8c72085f3b125a523bcf93fde2884da4608a
ko-shima <xxghapxx.poomomo@gmail.com>
2023-12-20 14:34:59 +0900

    Formオブジェクト記述修正・本番環境データベースのリセット

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,11 +3,27 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-    @order = OrderShipping.new
+    @order_shipping = OrderShipping.new
   end
 
   def create
     binding.pry
+    @order_shipping = OrderShipping.new(order_params)
+    if @order_shipping.valid?
+       @order_shipping.save
+       redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
+  private
+
+  def order_params
+    params.require(:order_shipping).permit(:postal_code, :region_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id)
+  end
+
+  #def shipping_params
+  #  params.permit(:postal_code, :region_id, :city, :block, :building, :phone_number)
+  #end
 end
