73c587bdb9a0d6022baa2a0488e3af3568f48bca
ko-shima <xxghapxx.poomomo@gmail.com>
2023-12-23 19:04:12 +0900

    郵便番号、電話番号のモデル修正（正規化）

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -5,11 +5,11 @@ class OrderShipping
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :region_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :block
-    validates :phone_number
+    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
   end
 
   def save
